645d7f4feb461ab1a58d7cfaeb316b29cf2643fa
NaghelyAguinsaca <135991677+NaghelyAguinsaca@users.noreply.github.com>
2023-06-16 04:20:58 +0000

    Tarea en Clase

---
+++ programa.py
@@ -0,0 +1,7 @@
+from modulo_funciones import sumar, restar, multiplicar, dividir
+from algebra import distancia 
+print(sumar(2, 3))
+print(restar(4, 2))
+print(multiplicar(5, 6))
+print(dividir(8, 4))
+print(distancia(1, 2, 3, 4))
